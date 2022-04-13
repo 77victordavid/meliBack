1fd60829366b3adc267784666a94ade6459948a0
Victor David <vdavid@proteccion.com.co>
2022-04-13 10:37:16 -0500

    fix: mejorara logica para las categorias

--- utils/utils.js
+++ utils/utils.js
@@ -1,7 +1,16 @@
-const { chain } = require("lodash");
+const { chain, has } = require("lodash");
 
 
- const getCategories = (list) => chain(list).orderBy(['results'], ['desc']).map(item=> item.name).value();
+ const getCategories = (list) => {
+   const findPath = list.find(l => has(l, 'path_from_root'));
+   let categories = [];
+   if(findPath) {
+    categories = chain(list[0].path_from_root).map(item=> item.name).value();
+   } else {
+     categories = chain(list).orderBy(['results', ['desc']]).map(item=> item.name).value();
+   }
+   return categories;
+ };
 
  module.exports = {
   getCategories,
